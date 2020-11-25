0f578eaad14dc62129ca5d18f83dc27195a355b3
Aleksandr <medovnikaleksander@yandex.ua>
2020-11-26 00:07:12 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573' />
+        <h1>Tip-Top</h1>
+      </header>
+    );
+};
+
+export default Header;
