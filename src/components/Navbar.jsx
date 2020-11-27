7c589b7763ba4963f7ff16d99f138c1e53f5c41e
Aleksandr <medovnikaleksander@yandex.ua>
2020-11-27 13:43:58 +0300

    Added css-classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
+import './Navbar.css'
+
 const Navbar = () => {
-    return (
-        <nav className='nav'>
-        <div><a href='#'>Profile</a></div>
-        <div><a href='#'>Messages</a></div>
-        <div><a href='#'>News</a></div>
-        <div><a href='#'>Settings</a></div>
-      </nav>
-    );
+  return (
+    <nav className='nav'>
+      <div><a href='#'>Profile</a></div>
+      <div><a href='#'>Messages</a></div>
+      <div><a href='#'>News</a></div>
+      <div><a href='#'>Settings</a></div>
+    </nav>
+  );
 };
 
 export default Navbar;
