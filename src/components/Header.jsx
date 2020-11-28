8e7d2a14ae10bcad6ab9bfbefa5c264f9c075fac
Aleksandr <medovnikaleksander@yandex.ua>
2020-11-29 01:21:02 +0300

    added style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,27 @@
 import classes from './Header.module.css';
 
 const Header = () => {
-    return (
-        <header className={classes.header}>
-        <img className={classes.header__logo} src='https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573' />
-        <h1 className={classes.header__h1}>Tip-Top</h1>
-      </header>
-    );
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <a href='#'>
+          <img className={`${classes.logo__img} ${classes.header__img}`} src='https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573' />
+        </a>
+        <h1><a href='#' className={classes.logo__link}>Tip-Top</a></h1>
+      </div>
+
+      <form className={classes.header__search}>
+        <input type='search' name="user" placeholder='Веедите поисковый запрос' autocomplete='on' className={classes.search} />
+      </form>
+
+      <a className={classes.shop} href='#'>shop</a>
+
+      <div className={classes.header__profile}>
+        <b>Mark Zuckerberg</b>
+        <img className={classes.header__img} src='https://3dnews.ru/assets/external/illustrations/2020/01/10/1001163/01.jpg' />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <ul className={classes.nav__menu}>
-        <li><a href='#'>Profile</a></li>
-        <li><a href='#'>Messages</a></li>
-        <li><a href='#'>News</a></li>
-        <li><a href='#'>Settings</a></li>
+        <li><a href='#' className={`${classes.nav__item} ${classes.active}`}>Profile</a></li>
+        <li><a href='#' className={classes.nav__item}>Messages</a></li>
+        <li><a href='#' className={classes.nav__item}>News</a></li>
+        <li><a href='#' className={classes.nav__item}>Settings</a></li>
       </ul>
     </nav>
   );
