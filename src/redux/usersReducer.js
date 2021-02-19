318c7ff1bd3d308e97194035e12e0730dd632394
Aleksandr <medovnikaleksander@yandex.ua>
2021-02-19 17:00:35 +0300

    Added class UserC

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -4,12 +4,7 @@ const SET_USERS = 'SET-USERS';
 
 let initialState = {
 
-    users: [
-        { fullName: 'Alex Rogin', id: 1, location: { city: 'Beijing', country: 'China' }, followed: true },
-        { fullName: 'Nataly Sylkina', id: 2, location: { city: 'Tokyo', country: 'Japan' }, followed: true },
-        { fullName: 'Dmitry Fox', id: 3, location: { city: 'Geneva', country: 'Switzerland' }, followed: false },
-        { fullName: 'Inna Goyna', id: 4, location: { city: 'Stockholm', country: 'Sweden' }, followed: false },
-    ]
+    users: []
 };
 
 const usersReducer = (state = initialState, action) => {
